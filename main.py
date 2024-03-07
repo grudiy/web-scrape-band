70202109e92b3dc9f1990297a90d0ec58077d250
gruand <gruand@gmail.com>
2024-03-07 18:07:20 +0200

    Implement OOP

--- main.py
+++ main.py
@@ -2,55 +2,66 @@ import time
 import requests
 import selectorlib
 import os
-from mailing import send_email
 import sqlite3
 
 URL = "http://programmer100.pythonanywhere.com/tours"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from the URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Email:
+    def send(self, msg):
+        # Just a mock of email sending function
+        print(msg)
+        print("Email was sent!")
 
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
-    connection.commit()
+class Database:
+    def __init__(self, db_path):
+        self.connection = sqlite3.connect(db_path)
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?, ?, ?)", row)
+        self.connection.commit()
 
-def read(extracted):
-    row_arg = extracted.split(",")
-    row_arg = [item.strip() for item in row_arg]
-    band, city, date = row_arg
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def read(self, extracted):
+        row_arg = extracted.split(",")
+        row_arg = [item.strip() for item in row_arg]
+        band, city, date = row_arg
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(db_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email("New events found!")
+                database.store(extracted)
+                email = Email()
+                email.send("New events found!")
         time.sleep(2)
 
